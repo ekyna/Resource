1d716705cd177e6571596ed253a730fbe33f8a8d
Etienne Dauvergne <contact@ekyna.com>
2022-05-06 15:24:11 +0200

    [Resource] Added Doctrine IdHydrator.

---
+++ Doctrine/ORM/Hydrator/IdHydrator.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ekyna\Component\Resource\Doctrine\ORM\Hydrator;
+
+use Doctrine\ORM\Internal\Hydration\AbstractHydrator;
+
+/**
+ * Class IdHydrator
+ * @package Ekyna\Component\Resource\Doctrine\ORM\Hydrator
+ * @author  Étienne Dauvergne <contact@ekyna.com>
+ */
+class IdHydrator extends AbstractHydrator
+{
+    public const NAME = 'IdHydrator';
+
+    protected function hydrateAllData(): array
+    {
+        return array_map(fn(string $id) => (int)$id, $this->statement()->fetchFirstColumn());
+    }
+}
